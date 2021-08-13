7a57d167ad37b2a186ae182bbdbc4ecaae5771e0
keleris32 <charles.ejiegbu@gmail.com>
2021-08-13 10:06:55 +0100

    Account settings email change alert text: changed from 'ok' to 'proceed'

--- src/screens/AccountSettings/index.js
+++ src/screens/AccountSettings/index.js
@@ -108,7 +108,7 @@ const AccountSettings = ({ navigation }) => {
             onPress: () => {},
           },
           {
-            text: 'Ok',
+            text: 'Proceed',
             onPress: () => updateRetailerProfile(formProp),
           },
         ],
