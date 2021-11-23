559fa1f428fbf7c071fb2a2e0308b497fdb10d49
keleris32 <charles.ejiegbu@gmail.com>
2021-11-23 07:47:58 +0100

    Deployed v1.3.1 for internal testing

--- src/components/AfrostreamComponent/SubscriptionPartnersComponent.js
+++ src/components/AfrostreamComponent/SubscriptionPartnersComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import Icons from 'react-native-vector-icons/FontAwesome';
