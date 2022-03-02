75f5f9dbbbbd4a6e0cedb34f7d14721a87682214
keleris32 <charles.ejiegbu@gmail.com>
2022-03-02 17:23:27 +0100

    chore: cleared redundant logic

--- src/screens/Payments/FlutterwaveGateway.js
+++ src/screens/Payments/FlutterwaveGateway.js
@@ -3,10 +3,12 @@ import { PayWithFlutterwave, FlutterwaveInit } from 'flutterwave-react-native';
 import EnvironmentVariables from '../../config/env';
 import { GlobalContext } from '../../context/Provider';
 
-const FlutterwaveGateway = async () => {
+const FlutterwaveGateway = async props => {
   const {
     getRetailerState: { retailerData },
   } = useContext(GlobalContext);
+
+  console.log(props.countryId, props.email);
 
   const {
     selectedCardState: {
@@ -33,41 +35,72 @@ const FlutterwaveGateway = async () => {
     return b.join('');
   };
 
-  return (
-    <>
-      <PayWithFlutterwave
-        onRedirect={handleOnRedirect}
-        options={{
-          tx_ref: generateTransactionRef(11),
-          authorization: EnvironmentVariables.FLUTTERWAVE_PUBLIC_KEY,
-          amount: isAfrocinemaActive
-            ? selectedAfrocinemaData.premier.discounted_charging_price
-            : selectedAfrostreamData.discounted_charging_price,
-          currency: isAfrocinemaActive
-            ? selectedAfrocinemaData.premier.charging_currency
-            : selectedAfrostreamData.charging_currency,
-          payment_options: 'card',
-          meta: {
-            retailer_id: retailerData.id,
-            plan_id: isAfrocinemaActive
-              ? selectedAfrocinemaData.id
-              : selectedAfrostreamData.id,
-            cancel_subscription_url: 'https://myafrostream.tv/user',
-            payment_purpose: isAfrocinemaActive
-              ? 'afrocinema_premier'
-              : 'afrostream_subscription',
-            customer_country_id: '06bc6944-2216-40c6-8772-796199e6c2ad',
-            customer_email: 'charles.ejiegbu@gmail.com',
-          },
-          customer: {
-            email: retailerData.email,
-            phone_number: retailerData.phone_number,
-            name: retailerData.first_name + ' ' + retailerData.last_name,
-          },
-        }}
-      />
-    </>
-  );
+  const paymentLink = await FlutterwaveInit({
+    tx_ref: generateTransactionRef(11),
+    authorization: EnvironmentVariables.FLUTTERWAVE_PUBLIC_KEY,
+    amount: isAfrocinemaActive
+      ? selectedAfrocinemaData.premier.discounted_charging_price
+      : selectedAfrostreamData.discounted_charging_price,
+    currency: isAfrocinemaActive
+      ? selectedAfrocinemaData.premier.charging_currency
+      : selectedAfrostreamData.charging_currency,
+    payment_options: 'card',
+    meta: {
+      retailer_id: retailerData.id,
+      plan_id: isAfrocinemaActive
+        ? selectedAfrocinemaData.id
+        : selectedAfrostreamData.id,
+      cancel_subscription_url: 'https://myafrostream.tv/user',
+      payment_purpose: isAfrocinemaActive
+        ? 'afrocinema_premier'
+        : 'afrostream_subscription',
+      customer_country_id: props.countryId,
+      customer_email: props.email,
+    },
+    customer: {
+      email: retailerData.email,
+      phone_number: retailerData.phone_number,
+      name: retailerData.first_name + ' ' + retailerData.last_name,
+    },
+  });
+
+  console.log('Linkkkk', paymentLink);
+
+  // return (
+  //   <>
+  //     <PayWithFlutterwave
+  //       onRedirect={handleOnRedirect}
+  //       options={{
+  //         tx_ref: generateTransactionRef(11),
+  //         authorization: EnvironmentVariables.FLUTTERWAVE_PUBLIC_KEY,
+  //         amount: isAfrocinemaActive
+  //           ? selectedAfrocinemaData.premier.discounted_charging_price
+  //           : selectedAfrostreamData.discounted_charging_price,
+  //         currency: isAfrocinemaActive
+  //           ? selectedAfrocinemaData.premier.charging_currency
+  //           : selectedAfrostreamData.charging_currency,
+  //         payment_options: 'card',
+  //         meta: {
+  //           retailer_id: retailerData.id,
+  //           plan_id: isAfrocinemaActive
+  //             ? selectedAfrocinemaData.id
+  //             : selectedAfrostreamData.id,
+  //           cancel_subscription_url: 'https://myafrostream.tv/user',
+  //           payment_purpose: isAfrocinemaActive
+  //             ? 'afrocinema_premier'
+  //             : 'afrostream_subscription',
+  //           customer_country_id: props.countryId,
+  //           customer_email: props.email,
+  //         },
+  //         customer: {
+  //           email: retailerData.email,
+  //           phone_number: retailerData.phone_number,
+  //           name: retailerData.first_name + ' ' + retailerData.last_name,
+  //         },
+  //       }}
+  //     />
+  //   </>
+  // );
 };
 
 export default FlutterwaveGateway;
